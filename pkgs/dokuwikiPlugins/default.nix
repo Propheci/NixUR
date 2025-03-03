21f1d8fcd6db9b6e71e32422f3977e8d29b21c02
Akshett Rai Jindal <jindalakshett@gmail.com>
2025-03-04 00:43:28 +0530

    feat(dokuwiki): add mathjax plugin

--- pkgs/dokuwikiPlugins/default.nix
+++ pkgs/dokuwikiPlugins/default.nix
@@ -5,6 +5,7 @@
     ditaa = pkgs.callPackage ./ditaa.nix {};
     edittable = pkgs.callPackage ./edittable.nix {};
     graphviz = pkgs.callPackage ./graphviz.nix {};
+    mathjax = pkgs.callPackage ./mathjax.nix {};
     move = pkgs.callPackage ./move.nix {};
     note = pkgs.callPackage ./note.nix {};
     wrap = pkgs.callPackage ./wrap.nix {};
